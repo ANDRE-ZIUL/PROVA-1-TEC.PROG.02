8d4288455be868151e58bc305882bdb2762d1f7c
André Luiz Cintra de Oliveira Dias <81429911+ANDRE-ZIUL@users.noreply.github.com>
2021-12-06 21:54:50 -0300

    Delete Prova02-TecProg2/src directory

--- Prova02-TecProg2/src/vo/CarData.java
+++
@@ -1,7 +0,0 @@
-package vo;
-
-public class CarData {
-	public String carBrand;
-	public String color;
-	public int yearOfManufacture;
-}
--- Prova02-TecProg2/src/vo/EngineData.java
+++
@@ -1,6 +0,0 @@
-package vo;
-
-public class EngineData {
-	public float potency;
-	public int cylinders;
-}
